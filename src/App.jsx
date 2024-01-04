ded260724a63430cc89f944af59064bb24a2dcb7
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-04 18:20:36 -0300

    ✨header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
-import "./styles.css";
+import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author="autor 1" content="conteudo 1" />
-      <Post author="autor 2" content="conteudo 2" />
-      <Post author="autor 3" content="conteudo 3" />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post author="autor 1" content="conteudo 1" />
+          <Post author="autor 2" content="conteudo 2" />
+          <Post author="autor 3" content="conteudo 3" />
+        </main>
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import ignieLogo from "../assets/ignite-logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={ignieLogo} alt="logo" />
     </header>
   );
 }
