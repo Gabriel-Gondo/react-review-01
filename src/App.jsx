48456d9f5e0a835b4741686fb62865370d4fea3b
Gabriel Gondo <gabriel.gondo@hitcode.com.br>
2024-01-03 21:46:47 -0300

    ⚰️removing unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from "./Post";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Post author="autor 1" content="conteudo 1" />
+      <Post author="autor 2" content="conteudo 2" />
+      <Post author="autor 3" content="conteudo 3" />
     </>
-  )
+  );
 }
-
-export default App
