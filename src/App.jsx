a9d2842879dab2ba7878058b98e47d7c86393b4f
Gabriel Gondo <gabriel.gondo@hitcode.com.br>
2024-01-03 21:57:09 -0300

    💄start styles css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./styles.css";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post author="autor 1" content="conteudo 1" />
       <Post author="autor 2" content="conteudo 2" />
       <Post author="autor 3" content="conteudo 3" />
