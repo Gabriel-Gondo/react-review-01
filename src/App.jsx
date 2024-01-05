8a04c1e86b4e80d3ed9c113d210b14c2ca806737
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-05 18:52:34 -0300

    💄sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Post } from "./Post";
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
+        <Sidebar />
         <main>
           <Post author="autor 1" content="conteudo 1" />
           <Post author="autor 2" content="conteudo 2" />
