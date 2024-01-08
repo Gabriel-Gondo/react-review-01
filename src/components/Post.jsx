fb4b0959cd1f8b200ad16e850f51e139c49d0243
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-08 18:26:45 -0300

    💄styling post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ export function Post() {
         <div className={styles.author}>
           <img
             className={styles.avatar}
-            src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://github.com/Gabriel-Gondo.png"
           />
           <div className={styles.authorInfo}>
             <strong>Gabriel Gondo</strong>
