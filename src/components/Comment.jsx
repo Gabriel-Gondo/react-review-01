358ba0e2ad8343e260b86a98c7e312430061290b
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-08 18:51:39 -0300

    ❇️comment section

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import styles from "./Comment.module.css";
+import { Trash, ThumbsUp } from "@phosphor-icons/react";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/Gabriel-Gondo.png"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Gondo</strong>
+              <time
+                title="sete de janeiro as 20:34"
+                dateTime="2024-01-07 20:34:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>texto</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
