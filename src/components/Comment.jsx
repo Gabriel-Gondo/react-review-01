057cb96e193270684590c7ee8a482c05b3ff1559
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-09 19:05:34 -0300

    💄comment style

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -22,7 +22,7 @@ export function Comment() {
               </time>
             </div>
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
